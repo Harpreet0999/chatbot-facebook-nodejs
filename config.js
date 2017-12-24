aa41d2838a482865fc54c487deba7a8300a18123
harpreet0999 <harpreetarora0999@gmail.com>
2017-12-24 19:28:48 +0530

    Initial

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
-    API_AI_CLIENT_ACCESS_TOKEN: '',
-    FB_APP_SECRET: '',
-    SERVER_URL: "",
+    FB_PAGE_TOKEN: 'EAACXYy9HmvEBADZAUkf8kRQmFNZBYWByefOZAzEZAwPcjoRyKvoRXomgR6jweCZAXepEYlozeV2QTOHfcnfdX8Awtwgc7LjjzyAXgG9oDBbtJR81Q67fCQ7rSzgWZBjYYVjoBfMv8UGMVJXFbwmwQQZAhNglgs5WVlZChwOZCMwxbJ96QA1bgCTus',
+    FB_VERIFY_TOKEN: 'my_fb_test_bot',
+    API_AI_CLIENT_ACCESS_TOKEN: '93e3d6103c4a4228a880d7c679643efa',
+    FB_APP_SECRET: '84edebe3348bcca9c1e3d8a878b64ded',
+    SERVER_URL: " https://test0999.herokuapp.com/",
 };
