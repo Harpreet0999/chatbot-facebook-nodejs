eb5ff74080c4655d42bf92867ffce822381b0967
harpreet0999 <harpreetarora0999@gmail.com>
2017-12-27 01:35:20 +0530

    waheguru

--- app.js
+++ app.js
@@ -503,42 +503,7 @@ function sendGenericMessage(recipientId, elements) {
 				type: "template",
 				payload: {
 					template_type: "generic",
-					elements: [
-          {
-            "title": "Deepesh Sodhi",
-            "image_url": "http://www.fluidonomics.com/wp-content/uploads/2016/05/sodhi.jpg",
-            "subtitle": "Ceo Fluidonomics",
-            "buttons": [
-              {
-                "type": "web_url",
-                "url": "https://www.linkedin.com/in/deepeshsodhi/",
-                "title": "Linkedin"
-              },
-              {
-                "type": "phone_number",
-                "title": "Call",
-                "payload": "<+919881203818>"
-              }
-            ]
-          },
-          {
-            "title": "Aditya Shastri",
-            "image_url": "https://media-exp1.licdn.com/mpr/mpr/shrinknp_200_200/p/2/000/07b/2d5/3377ec7.jpg",
-            "subtitle": "Ceo Fluidonomics",
-            "buttons": [
-              {
-                "type": "web_url",
-                "url": "https://www.linkedin.com/in/adishastri/",
-                "title": "Linkedin"
-              },
-              {
-                "type": "phone_number",
-                "title": "Call",
-                "payload": "<+919930880255>"
-              }
-            ]
-          }
-        ]
+					elements: elements
 				}
 			}
 		}
@@ -765,7 +730,7 @@ function receivedPostback(event) {
 		sendToApiAi(senderID,"Contact");
 		break;
 		case"Contact_me":
-		sendGenericMessage(senderID,elements);
+		sendGenericMessage(senderID,"Contact");
 		break;
 
 		case"participate":
