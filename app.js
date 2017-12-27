15d1ba557980ec7c936f1bad243cbacd4dc494ea
harpreet0999 <harpreetarora0999@gmail.com>
2017-12-27 22:25:19 +0530

    Bot tutorial

--- app.js
+++ app.js
@@ -766,7 +766,37 @@ function receivedPostback(event) {
 		break;
 		default:
 			//unindentified payload
-			sendTextMessage(senderID, "Will you please ellaborate ");
+			let elements=[
+		{
+			"title": "Deepesh Sodhi",
+            "image_url": "http://www.fluidonomics.com/wp-content/uploads/2016/05/sodhi.jpg",
+            "subtitle": "Ceo Fluidonomics",
+            "buttons": [
+              {
+                "type": "web_url",
+                "url": "https://www.linkedin.com/in/deepeshsodhi/",
+                "title": "Linkedin"
+              }
+            ]
+
+		},
+			{
+			"title": "Aditya Shastri",
+            "image_url": "https://media-exp1.licdn.com/mpr/mpr/shrinknp_200_200/p/2/000/07b/2d5/3377ec7.jpg",
+            "subtitle": "Ceo Fluidonomics",
+            "buttons": [
+              {
+                "type": "web_url",
+                "url": "https://www.linkedin.com/in/adishastri/",
+                "title": "Linkedin"
+              }
+              ]
+          }
+
+
+		];
+		sendGenericMessage(senderID,elements);
+			
 			break;
 
 	}
