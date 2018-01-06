1a5b0c7b7b7fc92f5716ff0c26c1052ef87f22d9
harpreet0999 <harpreetarora0999@gmail.com>
2018-01-06 13:14:59 +0530

    Fluidonomics

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    FB_PAGE_TOKEN: 'EAACXYy9HmvEBADZAUkf8kRQmFNZBYWByefOZAzEZAwPcjoRyKvoRXomgR6jweCZAXepEYlozeV2QTOHfcnfdX8Awtwgc7LjjzyAXgG9oDBbtJR81Q67fCQ7rSzgWZBjYYVjoBfMv8UGMVJXFbwmwQQZAhNglgs5WVlZChwOZCMwxbJ96QA1bgCTus',
+    FB_PAGE_TOKEN: 'EAACXYy9HmvEBAItuYnt5LgNM7yS6wxzWIZB2xr27W0txTZC3O8SVIZBOePB6hgkrnjU79MdS0CrsZA9S8C3pB5LZBuZC8XFJfaFQq5sNcnTPm5HKM84eugNbx0ZAZA8D7VX9vy1Q6qXhtpcVxWSXlLgZAyJH0OCumCfKMcSgCswe7Rect5AqUfbFV',
     FB_VERIFY_TOKEN: 'my_fb_test_bot',
     API_AI_CLIENT_ACCESS_TOKEN: '93e3d6103c4a4228a880d7c679643efa',
     FB_APP_SECRET: '84edebe3348bcca9c1e3d8a878b64ded',
@@ -14,4 +14,5 @@ module.exports = {
     	idleTimeoutMillis: 30000,
 
     },
+    
 };
