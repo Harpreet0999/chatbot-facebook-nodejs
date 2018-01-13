58ebddc59d1383d99bc46656844c1e1257ad79eb
harpreet0999 <harpreetarora0999@gmail.com>
2018-01-13 20:18:21 +0530

    Default

--- app.js
+++ app.js
@@ -880,7 +880,7 @@ function receivedPostback(event) {
 		
 
 		sendTextMessage(senderID,"Technology Solutions company with modern techno-creative fluid blend as its principle."+" Developing economically feasible, artistically adaptable, and technically cutting edge solutions as its focus.");
-fbService.sendTypingOn(sender),
+fbService.sendTypingOn(senderID),
 sendTextMessage(senderID, "Based on stage goals of organization we serve through choice of tracks. Acceleration, Build, Change, Direct and Economize"+" We promise no box-pushing, no hyper-specialization cacophony and no take your pick selling"+" We are a Perfect Fluid Blend of Art and Science");
 sendImageMessage(senderID,"http://beta.fluidonomics.com/wp-content/uploads/2016/05/know-1.jpg");
 		break;
